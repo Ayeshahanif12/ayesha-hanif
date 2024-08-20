03a7e57437198ecc92f05b108fd21fadd17f32fd
Ayeshhanif12 <ayeshahanif.0317@gmail.com>
2024-08-20 17:16:36 +0500

    work

--- game project/script.js
+++ game project/script.js
@@ -1,4 +1,8 @@
 let direction = {x : 0 , y : 0}
-let foodAudio = new Audio ('sfx_thumbnail_3.svg');
-let gameOverSound = new Audio ('game project/download.htm');
+const foodAudio = new Audio ('sfx_thumbnail_3.svg');
+const gameOverSound = new Audio ('game project/download.htm');
 
+//Game Function
+function main(ctime){
+    window.requestAnimationFrame(main)
+}
