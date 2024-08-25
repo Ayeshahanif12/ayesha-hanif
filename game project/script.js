30b5b52fc614ff784c659b1dfd0e24666b8f6086
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2024-08-25 18:23:10 +0500

    important work

---
+++ game project/script.js
@@ -0,0 +1,46 @@
+//Define html element 
+const board = document.getElementById('gameBoard');
+
+
+//define game variables 
+let snake= [{ x: 10, y: 10}];
+let food = generateFood();
+
+//draw game map,snake,food
+function draw() {
+    board.innerHTML ='';
+    drawSnake();
+}
+
+//draw snake 
+function drawSnake() {
+    snake.forEach(segment => {
+      const snakeElement = createGameElement('div',
+         'snake');  
+      setPosition(snakeElement,segment);
+      board.appendChild(snakeElement);
+    });
+}
+// create snake or food/div
+function createGameElement(tag,className){
+    const element = document.createElement(tag);
+    element.className = className ;
+    return element ;  
+}
+// set the position of snake and food
+function setPosition(element,position ){
+element.style.gridColumn = position.x;
+element.style.gridRow = position.y;
+}
+//testing
+//draw();
+
+//draw food function 
+ function drawFood(){
+    const foodElement = createGameElement('div','food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+ }
+
+
+ generateFood();
